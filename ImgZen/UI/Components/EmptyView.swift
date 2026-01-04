b37e9480a2e2c1c04c06ebba74dc2773dc76a347
Andreas Link <mail@linkandreas.de>
2026-01-04 09:46:28 +0100

    feat: Add `LaunchScreen.storyboard`

--- ImgZen/UI/Components/EmptyView.swift
+++ ImgZen/UI/Components/EmptyView.swift
@@ -23,7 +23,6 @@ struct EmptyView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 100)
-                .cornerRadius(22.0703125)
             Text(String(localized: "label.readyToSelectImages"))
                 .font(.body)
             VStack(spacing: 20) {
