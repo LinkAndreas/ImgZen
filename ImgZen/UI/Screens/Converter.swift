b182ea204be56991c6db0a8dee30960225960605
Andreas Link <mail@linkandreas.de>
2026-04-15 11:47:24 +0200

    feat: Adapt email

--- ImgZen/UI/Screens/Converter.swift
+++ ImgZen/UI/Screens/Converter.swift
@@ -89,7 +89,7 @@ struct Converter: View {
             }
             .mailComposer(
                 isPresenting: $sheet.isMailComposerPresented,
-                recipients: ["imgzen@linkandreas.de"],
+                recipients: ["feedback@linkandreas.de"],
                 subject: String(localized: "mail.feedbackSubject"),
                 body: String(localized: "mail.feedbackBody")
             )
